075830a666df76d64651d6fbe92c50d6ed11552d
Jaume Ferrarons <jaume.ferrarons@gmail.com>
2023-03-08 21:20:43 +0100

    formatting

--- gptclass/gpt_class.py
+++ gptclass/gpt_class.py
@@ -1,46 +1,4 @@
-from typing import Any
-import textwrap
-
-from .gpt_client import chat_completion
-
-
-def function_writer(name: str, n_parameters: int) -> str:
-    conversation = [
-        {
-            "role": "system",
-            "content": textwrap.dedent(
-                """You are a python code generator that returns the code that satisfies the provided description. The answer should contain no explanation about the code, how it is working or any description. The output should be just code.""".strip()
-            ),
-        },
-        {
-            "role": "user",
-            "content": f"write a function named '{name}' that receives {n_parameters} parameter(s)",
-        },
-    ]
-    generated_code = chat_completion(conversation)
-    if "```python" in generated_code:
-        cleaned_code = generated_code.split("```python")[1].split("```")[0].strip()
-    return cleaned_code
-
-
-def function_caller(name: str):
-    def f(*args):
-        code = function_writer(name, len(args))
-        locals: dict[str, Any] = {}
-        exec(code, globals(), locals)
-        for key, value in locals.items():
-            globals()[key] = value
-        func = locals[name]
-        return func(*args)
-
-    return f
-
-
-def function_explainer(name: str):
-    def f(*args):
-        print(function_writer(name, len(args)))
-
-    return f
+from .function_utils import function_explainer, function_caller
 
 
 class Explainer:
@@ -50,7 +8,7 @@ class Explainer:
 
 class GPTClass:
     @property
-    def explain(self):
+    def explain(self) -> Explainer:
         """Prints the code of the generated method
         Example: `GPTClass().explain.add(4, 5)`
 
--- tests/test_gpt_class.py
+++ tests/test_gpt_class.py
@@ -6,16 +6,18 @@ from gptclass import GPTClass
 
 
 class GPTClassTest(unittest.TestCase):
-    def test_addition(self):
+    @patch("sys.stdout", new_callable=StringIO)
+    def test_addition(self, stdout):
         gpt = GPTClass()
         self.assertEqual(gpt.add(4, 5), 9)
         self.assertEqual(gpt.add(2, 3), 5)
+        self.assertEqual(stdout.getvalue(), "")
 
     @patch("sys.stdout", new_callable=StringIO)
     def test_explain_addition(self, stdout):
         gpt = GPTClass()
         gpt.explain.add(4, 5)
-        self.assertEqual(stdout.getvalue(), "def add(a, b):\n    return a + b\n")
+        self.assertEqual(stdout.getvalue(), "def add(a,b):\n    return a+b\n")
 
     def test_recursive_method(self):
         gpt = GPTClass()
